0b9d03cc304af78bdf23072ac6ee5a0a51c9ec4d
Krishnapsd86 <foreverkris@yahoo.com>
2023-03-24 21:45:38 -0500

    update

--- src/Collection/Day4Iterator.java
+++ src/Collection/Day4Iterator.java
@@ -20,24 +20,36 @@ public class Day4Iterator {
 		// looping to collection
 		while (it.hasNext()) {
 			System.out.println(it.next());
+		}
+		System.out.println("+++++++++++++++++++++++++++++++++++");
+		
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		numbers.add(40);
+		numbers.add(8);
+		numbers.add(20);
+		numbers.add(15);
+		numbers.add(5);
+		numbers.add(18);
 
-			ArrayList<Integer> numbers = new ArrayList<Integer>();
-			numbers.add(10);
-			numbers.add(8);
-			numbers.add(5);
-			numbers.add(18);
+		for (int i = 0; i < numbers.size(); i++) {
+			if (numbers.get(i) < 15) {
+				numbers.remove(numbers.get(i));
+				i = i - 1;
+				continue;
+			}
+			System.out.println(numbers.get(i));
+		}
+		
 
 			Iterator<Integer> num = numbers.iterator();
 
 			while (num.hasNext()) {
-				Integer i = num.next();
-				if (i < 10) {
+				//Integer i = num.next();
+				if (num.next()< 10) {
 					num.remove();
 				}
 
 			}
 			System.out.println(numbers);
-		}
 	}
-
 }
